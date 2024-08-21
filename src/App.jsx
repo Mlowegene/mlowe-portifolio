ce107ec6168aa822cb452eec59b279ed217d9436
Mlowegene <genemlowe@gmail.com>
2024-08-21 12:06:39 +0300

    initialize project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import NavBar from './components/NavBar'
+import Hero from './components/Hero'
+
+function App() {
+
+  return (
+    <>
+    <NavBar />
+    <Hero />
+    </>
+  )
+}
+
+export default App
