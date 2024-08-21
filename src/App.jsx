7d3f134b882977f11f7a8a01bce853f66ad64fef
Mlowegene <genemlowe@gmail.com>
2024-08-21 16:08:18 +0300

    feat: added contact section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Service from './components/Service'
 import Projects from './components/Projects'
+import Contact from './components/Contact'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
     <About />
     <Service />
     <Projects />
+    <Contact />
     </>
   )
 }
