ff9c6e603e99108759d61fccb4d00dba405a19b1
Mlowegene <genemlowe@gmail.com>
2024-08-21 16:15:09 +0300

    fix typo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -29,7 +29,7 @@ const projects = [
 function Projects() {
     
   return (
-    <div className="bg-black text-white py-20 " id="service">
+    <div className="bg-black text-white py-20 " id="projects">
       <div className="container mx-auto px-8 md:px-16 lg:px-24 ">
         <h2 className="text-4xl font-bold text-center m-12">Projects</h2>
         <div className="grid drid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
--- src/components/Service.jsx
+++ src/components/Service.jsx
@@ -23,7 +23,7 @@ function Service() {
         }
     ]
   return (
-    <div className="bg-black text-white py-20 " id="service">
+    <div className="bg-black text-white py-20 " id="services">
       <div className="container mx-auto px-8 md:px-16 lg:px-24 ">
         <h2 className="text-4xl font-bold text-center m-12">My Service</h2>
         <div className="grid drid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
