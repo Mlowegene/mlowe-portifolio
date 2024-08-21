962335ac30550e7a080ba8814e327f07f021651a
Mlowegene <genemlowe@gmail.com>
2024-08-21 12:21:39 +0300

    change colors

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,7 +15,7 @@ function Hero() {
         <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white
             hidden md:inline transform transition-transform duration-300 hover:scale-105 
             px-4 py-2 rounded-full">Contact with me</button>
-        <button className="bg-gradient-to-r from-green-400 to-blue-500 text-white
+        <button className="bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             hidden md:inline transform transition-transform duration-300 hover:scale-105 
             px-4 py-2 rounded-full">Resume</button>
       </div>
