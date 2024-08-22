f6fbf9f5e02c131e4a11d57f96edfc10b83b2ed4
Mlowegene <genemlowe@gmail.com>
2024-08-22 11:59:37 +0300

    feat: added form functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,30 @@
-import React from 'react'
+import { useRef, useState, useEffect } from 'react';
+import  emailjs from '@emailjs/browser';
 import { FaEnvelope, FaMapMarkedAlt, FaPhone } from 'react-icons/fa'
 
 const Contact = () => {
+    const form = useRef();
+    const [isSent, setIsSent] = useState(false);
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        const YOUR_SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
+        const YOUR_TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
+        const YOUR_PUBLIC_KEY = import.meta.env.VITE_PUBLIC_KEY;
+
+        emailjs.sendForm(YOUR_SERVICE_ID, YOUR_TEMPLATE_ID, form.current, {
+            publicKey: YOUR_PUBLIC_KEY,
+          })
+          .then((result) => {
+              console.log("result",result.text);
+              setIsSent(true); // Show success message
+              setTimeout(() => setIsSent(false), 30000); // Hide message after 30 seconds
+          }, (error) => {
+              console.log(error);
+          });
+      };
+
   return (
     <div className="bg-black text-white py-20" id="contact">
       <div className="container mx-auto px-8 md:px-16 lg:px-24">
@@ -27,24 +50,24 @@ const Contact = () => {
             </div>
           </div>
           <div className='flex-1 w-full'>
-            <form className='space-y-4'>
+            <form ref={form} onSubmit={sendEmail} className='space-y-4'>
                 <div>
                     <label htmlFor="name" className='block mb-2'>Your Name</label>
-                    <input type="text" 
+                    <input type="text" name='user_name'
                     className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
                     focus:border-green-400'
                     placeholder='Enter You Name'/>
                 </div>
                 <div>
                     <label htmlFor="emial" className='block mb-2'>Email</label>
-                    <input type="text" 
+                    <input type="text" name='user_email'
                     className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
                     focus:border-green-400'
                     placeholder='Enter You Email'/>
                 </div>
                 <div>
                     <label htmlFor="message" className='block mb-2'>Message</label>
-                    <textarea type="text" 
+                    <textarea type="text" name='message'
                     className='w-full p-2 rounded bg-gray-800 border border-gray-600 focus:outline-none
                     focus:border-green-400'
                     rows="5"
@@ -53,6 +76,11 @@ const Contact = () => {
                 <button className='bg-gradient-to-r from-green-400 to-blue-500 text-white hidden md:inline
             transform transition-transform duration-300 hover:scale-105 px-8 py-2 rounded-full'>Send</button>
             </form>
+            {isSent && (
+                 <div className='mt-4 p-4 bg-green-500 text-white rounded'>
+                 Email sent successfully!
+                </div>
+            )}
           </div>
         </div>
       </div>
