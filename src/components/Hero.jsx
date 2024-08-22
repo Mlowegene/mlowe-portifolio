09f8c6c9984d3698bc5c562b2df367226d95aefe
Mlowegene <genemlowe@gmail.com>
2024-08-22 15:46:00 +0300

    feat: added resume download functionality

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,16 @@
 import mloweImage from "../assets/mlowe.jpg"
 
 function Hero() {
+
+  const handleDownload = () => {
+    const link = document.createElement('a');
+    link.href = '/resume.pdf'; // Path to your PDF file in the public folder
+    link.download = 'My_Resume.pdf'; // The name for the downloaded file
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
   return (
     <div className="bg-black text-white text-center py-16">
       <img src={mloweImage} alt="mlowe image" className="mx-auto mb-8 h-48 rounded-full 
@@ -15,7 +25,7 @@ function Hero() {
         <button className="bg-gradient-to-r from-[#2E3192] to-[#1BFFFF] text-white
             hidden md:inline transform transition-transform duration-300 hover:scale-105 
             px-4 py-2 rounded-full">Contact with me</button>
-        <button className="bg-gradient-to-r from-[#EA8D8D] to-[#A890FE] text-white
+        <button onClick={handleDownload} className="bg-gradient-to-r from-[#EA8D8D] to-[#A890FE] text-white
             hidden md:inline transform transition-transform duration-300 hover:scale-105 
             px-4 py-2 rounded-full">Resume</button>
       </div>
